8f3b142ef6f2b8b127c4878f9dc555437a77cc90
Akbar Muhamad Afriansyah <itsme@akbarafriansyah.my.id>
2021-12-12 20:03:43 +0700

    clear unused component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import { useContext, useEffect } from "react";
-import { Button, Card } from "react-bootstrap";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { AppContext } from "./Components/context/GlobalContext";
 import FormLogin from "./Components/FormLogin.jsx";
